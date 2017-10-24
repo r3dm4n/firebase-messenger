a005041d7f541038e64958ae8b45445f7a475bb7
Alexandru Corut <r3dm4n@me.com>
2017-10-24 17:09:28 +0100

    added UITableViewController

---
+++ firebase-messenger/Controller/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  firebase-messenger
+//
+//  Created by r3d on 24/10/2017.
+//  Copyright © 2017 Alexandru Corut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Firebase Chat App"
+
+    }
+
+ 
+
+
+}
+
