2bb06ca9f1cb0596c62c3147cd06ba8d67ee9deb
Alexandru Corut <r3dm4n@me.com>
2017-10-25 09:29:32 +0100

    added NewMessageController class

---
+++ firebase-messenger/Controller/NewMessageController.swift
@@ -0,0 +1,21 @@
+ //
+//  NewMessageController.swift
+//  firebase-messenger
+//
+//  Created by r3d on 25/10/2017.
+//  Copyright © 2017 Alexandru Corut. All rights reserved.
+//
+
+import UIKit
+
+class NewMessageController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+   
+
+}
