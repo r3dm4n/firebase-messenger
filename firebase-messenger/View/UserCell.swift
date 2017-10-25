64169c7f1244ca230a8643ef0b2dc67e2c24c261
Alexandru Corut <r3dm4n@me.com>
2017-10-25 13:19:50 +0100

    init cell

--- firebase-messenger/View/UserCell.swift
+++ firebase-messenger/View/UserCell.swift
@@ -10,15 +10,11 @@ import UIKit
 
 class UserCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
 }
