091cfa8fda0b8dd9ed97112a1f668716801f6600
Alexandru Corut <r3dm4n@me.com>
2017-10-25 13:14:47 +0100

    added UserCell

---
+++ firebase-messenger/View/UserCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserCell.swift
+//  firebase-messenger
+//
+//  Created by r3d on 25/10/2017.
+//  Copyright © 2017 Alexandru Corut. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
