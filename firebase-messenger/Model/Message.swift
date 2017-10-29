c9887c6a6530f8894b317cedfae0b7b753cd54a1
Alexandru Corut <r3dm4n@me.com>
2017-10-29 13:37:03 +0000

    added Message object

---
+++ firebase-messenger/Model/Message.swift
@@ -0,0 +1,16 @@
+//
+//  Message.swift
+//  firebase-messenger
+//
+//  Created by r3d on 29/10/2017.
+//  Copyright © 2017 Alexandru Corut. All rights reserved.
+//
+
+import Foundation
+
+class Message: NSObject {
+    var fromId: String?
+    var toId: String?
+    var text: String?
+    var timestamp: String?
+}
