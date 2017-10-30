75b5ac098b6335b0ab3c149cdbae92638cb67a5d
Alexandru Corut <r3dm4n@me.com>
2017-10-30 18:08:14 +0000

    added ChatMessageCell

---
+++ firebase-messenger/View/ChatMessageCell.swift
@@ -0,0 +1,31 @@
+//
+//  ChatMessageCell.swift
+//  firebase-messenger
+//
+//  Created by r3d on 30/10/2017.
+//  Copyright © 2017 Alexandru Corut. All rights reserved.
+//
+
+import UIKit
+
+class ChatMessageCell: UICollectionViewCell {
+    
+    let textView: UITextView = {
+        let tv = UITextView()
+        tv.text = "123"
+        tv.font = UIFont.systemFont(ofSize: 16)
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        return tv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .red
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
